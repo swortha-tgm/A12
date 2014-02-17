0987bb674feb2f7ccadbc3197ce3c73774fb28c5
Patrick Malik <austria.p@hotmail.com>
2014-02-17 20:37:14 +0100

    Meine Testklasse geaddet und setSecretAlphabet implementiert

--- A12/src/kritzlmalikwortha/MonoalphabeticCipher.java
+++ A12/src/kritzlmalikwortha/MonoalphabeticCipher.java
@@ -1,15 +1,19 @@
 package kritzlmalikwortha;
 
-public class MonoalphabeticCipher implements Cipher {
+import java.util.HashSet;
 
-	private String secretAlphabet;
+public class MonoalphabeticCipher implements Cipher{
+
+	private String secretAlphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
 
 	public MonoalphabeticCipher() {
-
+		
+		this.secretAlphabet=secretAlphabet;
+		
 	}
 
 	public String getSecretAlphabet() {
-		return null;
+		return this.secretAlphabet;
 	}
 
 
@@ -17,6 +21,7 @@ public class MonoalphabeticCipher implements Cipher {
 	 * @see kritzlmalikwortha.Cipher#encrypt(kritzlmalikwortha.lang.String)
 	 */
 	public String encrypt(String text) {
+		String alph = "abcdefghijklmnopqrstuvwxyzäöüß";
 		return null;
 	}
 
@@ -25,7 +30,39 @@ public class MonoalphabeticCipher implements Cipher {
 	 * @see kritzlmalikwortha.Cipher#decrypt(kritzlmalikwortha.lang.String)
 	 */
 	public String decrypt(String text) {
+		String alph = "abcdefghijklmnopqrstuvwxyzäöüß";
 		return null;
+	}
+	
+	public void setSecretAlphabet(String alphabet) throws IllegalArgumentException{
+		String alph = "abcdefghijklmnopqrstuvwxyzäöüß";
+		
+
+		if(alphabet==null){
+			throw new IllegalArgumentException("alphabet cannot be NULL");
+		}
+		
+		if(alphabet.length()!=30){
+			throw new IllegalArgumentException("The alphabet contains to many characters");
+		}
+		
+		for(int i = 0;i<this.secretAlphabet.length();i++){
+			
+			if(!alph.contains(""+alphabet.charAt(i))){
+				throw new IllegalArgumentException("The character '"+alphabet.charAt(i)+"' with the index "+i+" is NOT allowed");				
+			}
+		}
+		
+		HashSet<Character> set = new HashSet<Character>();
+		
+		for(int i = 0;i<this.secretAlphabet.length();i++){
+			set.add(alphabet.charAt(i));
+		}
+		if(set.size()!=30){
+			throw new IllegalArgumentException("There is at least one character ocurring at least two times, no duplicates allowed");
+		}
+		
+		this.secretAlphabet=alphabet;
 	}
 
 }
---
+++ A12/src/kritzlmalikwortha/TestMonoAlphabetic.java
@@ -0,0 +1,39 @@
+package kritzlmalikwortha;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMonoAlphabetic {
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void alph1() {
+		MonoalphabeticCipher a = new MonoalphabeticCipher();
+		a.setSecretAlphabet("a");
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void alph2() {
+		MonoalphabeticCipher a = new MonoalphabeticCipher();
+		a.setSecretAlphabet("aabcdefghijklmnopqrstuvwxyzäüö");
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void alph3() {
+		MonoalphabeticCipher a = new MonoalphabeticCipher();
+		a.setSecretAlphabet("abcdefghijklmnopqrstuvwxyzäüößa");
+	}
+
+	 @Test
+	 public void alph4() {
+		 MonoalphabeticCipher a = new MonoalphabeticCipher();
+		 a.setSecretAlphabet("acbdefghijklmnopqrstuvwxyzäüöß");
+		 assertEquals("acbdefghijklmnopqrstuvwxyzäüöß",a.getSecretAlphabet());
+	 }
+	 
+	 @Test
+	 public void alph5(){
+		 MonoalphabeticCipher a = new MonoalphabeticCipher();
+		 assertEquals("abcdefghijklmnopqrstuvwxyzäöüß",a.getSecretAlphabet());
+	 }
+}
