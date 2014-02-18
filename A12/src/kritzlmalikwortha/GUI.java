e9fc5503d3583053bc5514a0a5af050f6cae1f24
tgm-swortha <swortha@student.tgm.ac.at>
2014-02-18 07:48:11 +0100

    GUI updated StartCipher and Controller added

--- A12/src/kritzlmalikwortha/GUI.java
+++ A12/src/kritzlmalikwortha/GUI.java
@@ -1,5 +1,34 @@
 package kritzlmalikwortha;
 
-public class GUI {
+import javax.swing.*;
 
+/**
+ * Die GUI für das Verschlüsselungsprogramm wid hier erstellt!
+ * @author Simon Wortha
+ * @version 17.02.2014
+ *
+ */
+public class GUI extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextArea eingText;
+	private JTextArea ausgText;
+	private JLabel label;
+	private JButton bVer;
+	private JButton bEnt;
+	private JComboBox<String> dropdown;
+	private Controller controller;
+	/**
+	 * @param c 
+	 * 
+	 */
+	public GUI(Controller c) {
+		super("Das sneaky Verschlüsselungsprogramm");
+		this.controller = c;
+		
+		
+		
+	}
 }
