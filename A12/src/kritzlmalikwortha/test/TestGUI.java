f8d9d4331ab938ae9de0e14d0828e3085e4e7acc
Patrick Malik <austria.p@hotmail.com>
2014-02-19 16:22:58 +0100

    GUI geändert

--- A12/src/kritzlmalikwortha/test/TestGUI.java
+++ A12/src/kritzlmalikwortha/test/TestGUI.java
@@ -2,13 +2,24 @@ package kritzlmalikwortha.test;
 
 import static org.junit.Assert.*;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+
+import kritzlmalikwortha.Controller;
+import kritzlmalikwortha.GUI;
+
 import org.junit.Test;
 
 public class TestGUI {
 
 	@Test
 	public void testButtonVerPressed() {
-		fail("Not yet implemented");
+		GUI g = new GUI(new Controller());
+		JButton bVer = new JButton("Verschlüsseln");
+		ActionEvent a = new ActionEvent(bVer,1,null);
+		assertTrue(g.buttonVerPressed(a));
+		
 	}
 
 }
